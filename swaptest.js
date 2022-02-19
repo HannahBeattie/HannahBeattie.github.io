288f866205c9a253c27bdb9c12e11b52d9907418
Hannah Beattie <hannahdbeattie@gmail.com>
2022-02-19 23:38:57 +1300

    'on click' experement

---
+++ swaptest.js
@@ -0,0 +1,11 @@
+
+
+
+function change () {
+    var img=document.getElementById("myimg");
+    if (img.src === "images/lazerKitty.png")
+      img.src="images/lazerKitty.png";
+    else 
+      img.src="images/lazerKittyswap.png"
+}
+
