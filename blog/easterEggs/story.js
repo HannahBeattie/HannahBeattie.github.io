31948897c72e17b8f7a31d86b11a2899acf15989
Hannah Beattie <hannahdbeattie@gmail.com>
2022-03-11 14:21:27 +1300

    lonelyhearts

--- blog/easterEggs/story.js
+++ blog/easterEggs/story.js
@@ -8,25 +8,27 @@ function libIt() {
 	var noun = document.getElementById('noun').value
 	var liquid = document.getElementById('liquid').value
 	var noun2 = document.getElementById('noun2').value
-	var adjective3 = document.getElementById('adjective3').value
+	var noun3 = document.getElementById('noun3').value
 	storyDiv.innerHTML =
 		'Hello, my name is ' +
 		name +
 		', ' +
 		'<br>' +
-		'I enjoy long, ' +
+		'I enjoy long, firm, ' +
 		adjective +
 		' walks on the beach, getting ' +
 		verb +
-		' in the rain and ' +
+		' by a ' +
 		adjective2 +
-		' encounters with ' +
+		', and encounters with ' +
 		noun +
-		'. I really like ' +
+		'.' +
+		'<br>' +
+		'I really like ' +
 		liquid +
-		' mixed with ' +
+		' mixed with a good amount of ' +
 		noun2 +
-		', and romantic, candle-lit ' +
-		adjective3 +
+		', and expensive, candle-lit ' +
+		noun3 +
 		's.'
 }
